69ae047c2e1f48fbb7429beac52d9e84f55c8f91
ym534 <ym534766545@outlook.com>
2023-06-10 16:47:37 +0800

    feat(*):初始化项目

---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>home page</div>
+</template>
+<script>
+export default {
+  name: "home",
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {},
+};
+</script>
+<style scoped></style>
